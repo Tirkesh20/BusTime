acb534d346a212d43d9a02f52717693097a74feb
tirkesh_yagshymyradov <tirkeshy@gmail.com>
2021-04-02 21:14:03 +0300

    added functions (milliToLocal,toMillis)

--- src/main/java/TimeConverter.java
+++ src/main/java/TimeConverter.java
@@ -3,6 +3,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 
 public class TimeConverter {
@@ -10,14 +11,23 @@ public class TimeConverter {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         return  LocalTime.parse(time, formatter);
     }
-    public Long toMillis(LocalTime time){
-
-
+    public Long toMillis() throws ParseException {
+        String string_date = "10:00";
+        SimpleDateFormat f = new SimpleDateFormat("HH:mm");
+        try {
+            Date d = f.parse(string_date);
+             long milliseconds = d.getTime();
+             return milliseconds;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+       return null;
     }
     public LocalTime milliToLocal(Long epoch){
        LocalDateTime ldt = Instant.ofEpochMilli(epoch)
                 .atZone(ZoneId.systemDefault()).toLocalDateTime();
         LocalTime localTime=ldt.toLocalTime();
+        System.out.println(localTime);
         return localTime;
     }
 }
